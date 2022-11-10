6dea5734a9e92e3518ebb5505e1f26eee096a659
thalber <73020111+thalber@users.noreply.github.com>
2022-11-10 20:07:38 +0400

    Add comments, add nullable forcing, add editorconfig

--- tests/Program.cs
+++ tests/Program.cs
@@ -9,12 +9,12 @@ internal class Program
 
     internal static void Main()
     {
-        TwoPools<int, int> 
+        TwoPools<int, int>
             test1 = new(new[] { 1, 2, 3 }, new[] { 4, 5, 6 }),
-            test2 = new(new[] {1, 2, 3}, new[] { 7, 8, 9 });
+            test2 = new(new[] { 1, 2, 3 }, new[] { 7, 8, 9 });
         test1.AddLink(1, 4);
         test2.AddLink(1, 8);
-        var test3 = TwoPools<int, int>.Stitch(test1, test2);
+        TwoPools<int, int>? test3 = TwoPools<int, int>.Stitch(test1, test2);
         Console.WriteLine(test3);
     }
 }
